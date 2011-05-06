d8e66e9a9b8544de6d0c396f3c4f4cce7995471a
Ari Russo <ari.russo@gmail.com>
2011-05-05 21:47:31 -0400

    first

---
+++ lib/topaz.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+#
+# MIDI Tempo + Ruby
+# (c)2011 Ari Russo and licensed under the Apache 2.0 License
+# 
+
+require 'gamelan'
+require 'midi-message'
+require 'nibbler'
+require 'unimidi'
+
+module Topaz
+  
+  
+end
