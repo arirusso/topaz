c7ab8c9ab26889aada248f019e6a00bede5ffb06
Ari Russo <ari.russo@gmail.com>
2011-05-06 01:06:00 -0400

    cleanup

--- lib/topaz/tempo.rb
+++ lib/topaz/tempo.rb
@@ -18,31 +18,31 @@ module Topaz
              
       @scheduler = Gamelan::Scheduler.new({:tempo => tempo})
       
-      schedule_midi_events
+      initialize_midi
     end
     
     def start
-      each_midi_output { |o| o.puts(SystemCommon["Start"].to_bytes) }
+      output_midi { |o| o.puts(SystemCommon["Start"].to_bytes) }
       @scheduler.run
       @scheduler.join
     end
 
     def stop
-      each_midi_output { |o| o.puts(SystemCommon["Stop"].to_bytes) }
+      output_midi { |o| o.puts(SystemCommon["Stop"].to_bytes) }
       @scheduler.stop
     end
         
     private
     
-    def each_midi_output(&task)
+    def output_midi(&task)
       @outputs[:midi].each do |output|
         task.call(output)
       end      
     end
     
-    def schedule_midi_events
+    def initialize_midi
       @scheduler.at(16) do
-        each_midi_output { |o| o.puts(SystemCommon["Continue"].to_bytes) }       
+        output_midi { |o| o.puts(SystemCommon["Continue"].to_bytes) }       
       end
     end
         
