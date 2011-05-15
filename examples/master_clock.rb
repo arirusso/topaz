3a3d4549d99132929c0eac4c9b49b60883d7a8d9
Ari Russo <ari.russo@gmail.com>
2011-05-14 20:17:47 -0400

    cleanup example

--- examples/master_clock.rb
+++ examples/master_clock.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
+
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 dir = File.dirname(File.expand_path(__FILE__))
-$LOAD_PATH.unshift dir + '/../../alsa-rawmidi/lib'
 $LOAD_PATH.unshift dir + '/../../midi-eye/lib'
 
 require 'topaz'
@@ -11,8 +11,6 @@ input = UniMIDI::Input.first.open
 output = UniMIDI::Output.first.open
 
 @tempo = Topaz::Tempo.new(132, :midi_input => input, :midi_output => output)
-
-#@tempo.at(64) { @tempo.stop }
   
 @tempo.wait_for_midi_start
 
