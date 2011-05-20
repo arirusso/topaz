a2fa6b84801ec90f88c6eae237bc1231ca04a51d
Ari Russo <ari.russo@gmail.com>
2011-05-19 22:06:21 -0400

    comments, example

--- lib/topaz/tempo.rb
+++ lib/topaz/tempo.rb
@@ -16,16 +16,26 @@ module Topaz
       @on_step = event
     end
     
+    # this will effectively reinitialize, switching from internal to external midi or vice versa 
+    def switch_source
+      
+    end
+    
     # this will change the tempo
+    #
     # in the case that you are using external midi tempo, this will switch to internal tempo
     # at the desired rate
+    #
     def tempo=(val)
       # if @source.respond_to?(:tempo=)
       # etc 
     end
     
     # this will start the generator
-    # in the case that you plan on using external midi tempo, this will wait for a start message
+    #
+    # in the case that you plan on using external midi tempo, this will wait for a start 
+    # or clock message
+    #
     def start(options = {})
       
     end
