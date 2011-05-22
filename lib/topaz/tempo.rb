1b1a8c3573224c3ed149ecf8c70c7583d83ffdf7
Ari Russo <ari.russo@gmail.com>
2011-05-22 19:04:15 -0400

    output sync

--- lib/topaz/tempo.rb
+++ lib/topaz/tempo.rb
@@ -7,15 +7,20 @@ module Topaz
     
     attr_reader :source
     
-    def_delegators :source, :tempo
+    def_delegators :source, :tempo, :interval, :interval=
   
-    def initialize(*a, &event)
-      options = a.last.kind_of?(Hash) ? a.last : {}  
-      @action = { :on_tick => event }
-      @source = case a.first
-        when Numeric then InternalTempo.new(a.first, @action, options)
-        when Hash then ExternalMIDITempo.new(a.first[:midi], @action, options)
+    def initialize(*args, &event)
+      @destinations = []
+      
+      if args.first.kind_of?(Numeric)
+        @source = InternalTempo.new(args.shift)
       end
+      options = args.first
+      
+      initialize_midi_io(options)
+      raise "You must specify an internal tempo rate or an external tempo source" if @source.nil?
+      @source.action = { :on_tick => event, :destinations => @destinations }
+      @source.interval = options[:interval] unless options[:interval].nil? 
     end
     
     # this will change the tempo
@@ -31,19 +36,46 @@ module Topaz
       end 
     end
     
+    def on_start(&block)
+      @on_start = block
+    end
+
+    def on_stop(&block)
+      @on_stop = block
+    end
+        
     # this will start the generator
     #
     # in the case that external midi tempo is being used, this will wait for a start 
     # or clock message
     #
     def start(options = {})
+      @destinations.each { |dest| dest.on_start }
+      @on_start.call unless @on_start.nil?
       @source.start(options)
     end
     
     # this will stop everything
     def stop(options = {})
+      @destinations.each { |dest| dest.on_stop }
+      @on_stop.call unless @on_stop.nil?
       @source.stop(options)
     end
+    
+    private
+        
+    def initialize_midi_io(args)
+      ports = args.kind_of?(Hash) ? args[:midi] : args
+      unless ports.nil?
+        if ports.kind_of?(Array) 
+          ports.each { |port| initialize_midi_io(port) }
+        elsif ports.type.eql?(:input) && @source.nil?
+          @source = ExternalMIDITempo.new(ports) 
+        elsif ports.type.eql?(:output)
+          @destinations << MIDISyncOutput.new(ports)
+        end
+      end
+    end    
     
   end
   
