ff8f807d525caf7cc5f698ba19a89677f34a9486
Ari Russo <ari.russo@gmail.com>
2011-05-22 23:33:48 -0400

    tests

--- test/test_internal_tempo.rb
+++ test/test_internal_tempo.rb
@@ -6,5 +6,23 @@ class InternalTempoTest < Test::Unit::TestCase
 
   include Topaz
   include TestHelper
+  
+  def test_stop_when
+    i = 0
+    count_to = 5
+    
+    tempo = Tempo.new(120) { i += 1 }
+    tempo.stop_when { i.eql?(count_to) }
+    tempo.start
+    
+    assert_equal(count_to, i)
+  end
+  
+  def test_timer
+    i = 0
+    count_to = 5
+    
+    tempo = Tempo.new(60) { i += 1 }
+  end
 
 end
