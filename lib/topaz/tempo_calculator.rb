02176b7a63598ce3039a20c386d101f1e3d8ff3b
Ari Russo <ari.russo@gmail.com>
2014-08-30 13:22:05 -0400

    threshold constant, comments

--- lib/topaz/tempo_calculator.rb
+++ lib/topaz/tempo_calculator.rb
@@ -1,21 +1,22 @@
-#!/usr/bin/env ruby
 module Topaz
   
+  # Calculate tempo given timestamps
   class TempoCalculator
+
+    THRESHOLD = 6 # minimum number of ticks to analyze
     
     attr_reader :tempo, :timestamps
     
-    def initialize(tempo = nil)
-      @tempo = tempo
+    def initialize
+      @tempo = nil
       @timestamps = []
-      @counter = 0
     end
     
-    # analyse the tempo based on the last 6 ticks
+    # Analyze the tempo based on the threshold
     def find_tempo
       tempo = nil
       diffs = []
-      @timestamps.shift while @timestamps.length > 6
+      @timestamps.shift while @timestamps.length > THRESHOLD
       @timestamps.each_with_index { |n, i| (diffs << (@timestamps[i+1] - n)) unless @timestamps[i+1].nil? }
       unless diffs.empty?
         avg = (diffs.inject { |a, b| a + b }.to_f / diffs.length.to_f) 
