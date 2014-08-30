6d966598ca920f5c5c1dda9d83abd6f90ba38ede
Ari Russo <ari.russo@gmail.com>
2014-08-30 13:21:13 -0400

    clearer event handling

--- lib/topaz/events.rb
+++ lib/topaz/events.rb
@@ -1,18 +1,42 @@
-#!/usr/bin/env ruby
 module Topaz
   
-  module TempoSource
+  # User defined callbacks for tempo events
+  class Events
+
+    attr_reader :tick
+    attr_accessor :midi_clock, :midi_start, :midi_stop, :start, :stop, :stop_when
+
+    def initialize()
+      @start = []
+      @stop = []
+      @tick = [] 
+
+      @midi_clock = nil
+      @midi_start = nil
+      @midi_stop = nil
+      @stop_when = nil
+    end
     
     def do_midi_clock
-      @actions[:midi_clock].call
+      @midi_clock.call
+    end
+
+    def do_start
+      @midi_start.call
+      @start.each(&:call)
+    end
+
+    def do_stop
+      @midi_stop.call
+      @stop.each(&:call)
     end
     
     def do_tick
-      @actions[:tick].call
+      @tick.each(&:call)
     end
     
     def stop?
-      !@actions[:stop_when].nil? && @actions[:stop_when].call
+      @stop_when.call unless @stop_when.nil?
     end
    
   end
