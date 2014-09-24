988067dccb42065a883a2a9401a4e029b8050f65
Ari Russo <ari.russo@gmail.com>
2014-09-24 14:12:34 -0700

    clean up var names

--- lib/topaz/timer.rb
+++ lib/topaz/timer.rb
@@ -2,38 +2,50 @@ module Topaz
 
   class Timer < Gamelan::Timer
 
-    def initialize(event, tempo, options = {})
-      @event = event
-      @last = 0
-      @last_sync = 0
+    # @param [Fixnum] tempo
+    # @param [Hash] options
+    # @option options [Tempo::Event] :event
+    def initialize(tempo, options = {})
+      @event = options[:event]
+      @last_tick_event = 0
+      @last_midi_clock = 0
       self.interval = options[:interval] || 4 
       
       super({:tempo => tempo})
     end
 
-    # start the internal timer
-    # pass :background => true to keep the timer in a background thread
+    # Start the internal timer
+    # @param [Hash] options
+    # @option options [Boolean] :background Whether to run the timer in a background thread (default: false)
+    # @return [Timer]
     def start(options = {})
       run
       join unless !!options[:background]
       self
     end
     
-    # change the timer's click interval    
-    def interval=(val)
-      @interval = val / 4
+    # Set the timer's click interval
+    # @param [Fixnum] value
+    # @return [Fixnum]
+    def interval=(value)
+      @interval = value / 4
     end
     
+    # The timer's click interval
+    # @return [Fixnum]
     def interval
       @interval * 4
     end
     
-    # stop the timer
+    # Stop the timer
+    # @return [Timer]
     def stop(*a)
       super()
       self
     end
     
+    # Join the timer thread
+    # @return [Timer]
     def join
       super()
       self
@@ -41,48 +53,62 @@ module Topaz
     
     protected
 
-    # Initialize the scheduler's clock, and begin executing tasks.
+    # Initialize the scheduler's clock, and begin executing tasks
+    # @return [Boolean]
     def run
-      return if @running
-      @running  = true
-      @thread   = Thread.new do
-        begin
-          @phase  = 0.0
-          @origin = @time = Time.now.to_f
-          loop do 
-            dispatch 
-            advance
+      unless @running
+        @running = true
+        @thread = Thread.new do
+          begin
+            @phase  = 0.0
+            @origin = @time = Time.now.to_f
+            loop do 
+              dispatch 
+              advance
+            end
+          rescue Exception => exception
+            Thread.main.raise(exception)
           end
-        rescue Exception => exception
-          Thread.main.raise(exception)
         end
+        @thread.abort_on_exception = true
+        true
+      else
+        false
       end
-      @thread.abort_on_exception = true
     end 
 
     # Run all ready tasks.
+    # @return [Boolean]
     def dispatch
-      # stuff to do on every tick      
+      # Stuff to do on every tick      
       if time_for_midi_clock?
         # look for stop
-        @event.do_clock        
-        @last_sync = (@phase * 24).to_i
+        !@event.nil? && @event.do_clock        
+        @last_midi_clock = (@phase * 24).to_i
+        true
       end
-      # stuff to do on @interval
+      # Stuff to do on @interval
       if time_for_tick?
-        @event.do_tick
-        @last = (@phase * @interval).to_i
-      end      
+        !@event.nil? && @event.do_tick
+        @last_tick_event = (@phase * @interval).to_i
+        true
+      end
     end
     
     private
     
+    # Is the current phase appropriate for MIDI clock output?
+    # @return [Boolean]
     def time_for_midi_clock?
-      !@last_sync.eql?((@phase * 24).to_i)
+      phase = (@phase * 24).to_i
+      !@last_midi_clock.eql?(phase)
     end
     
+    # Is the current phase appropriate for the tick event?
+    # @return [Boolean]
     def time_for_tick?
-      !@last.eql?((@phase * @interval).to_i)
+      phase = (@phase * @interval).to_i
+      !@last_tick_event.eql?(phase)
     end
      
   end
