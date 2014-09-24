9a443c5edf4702bffe5d5b36518efaf7b887cac8
Ari Russo <ari.russo@gmail.com>
2014-09-24 16:28:07 -0700

    comments

--- lib/topaz/api.rb
+++ lib/topaz/api.rb
@@ -1,12 +1,18 @@
 module Topaz
 
+  # Shortcuts to give the clock a more convenient API
   module API
 
     def self.included(base)
       base.send(:extend, Forwardable)
       base.send(:def_delegators, :source, :interval, :interval=, :join, 
                 :pause, :pause?, :paused?, :running?, :tempo, :toggle_pause, :unpause)
-      base.send(:alias_method :time_since_start, :time)
+    end
+
+    # Alias for Clock#time
+    # @return [Time]
+    def time_since_start
+      time
     end
     
   end
