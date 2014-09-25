f5e7f2fb76299ae55f0fc95b71f7bf15a82b8037
Ari Russo <ari.russo@gmail.com>
2014-09-24 18:26:23 -0700

    better ivar names

--- lib/topaz/midi_clock_input.rb
+++ lib/topaz/midi_clock_input.rb
@@ -13,11 +13,11 @@ module Topaz
     # @option options [Clock::Event] :event
     def initialize(input, options = {})
       @event = options[:event]
-      @counter = 0
+      @tick_counter = 0
       @pause = false
       @running = false
       @tempo_calculator = TempoCalculator.new
-      @per_tick = interval_to_tick(options.fetch(:interval, 4))
+      @tick_threshold = interval_to_ticks(options.fetch(:interval, 4))
 
       initialize_listener(input)
     end
@@ -52,8 +52,8 @@ module Topaz
     end
         
     # Change the clock interval
-    # Defaults to click once every 24 ticks or one quarter note which is the MIDI standard.
-    # However, if you wish to fire the on_tick event twice as often, pass 8 
+    # Defaults to 4, which means click once every 24 ticks or one quarter note (per MIDI spec).
+    # Therefore, to fire the on_tick event twice as often, pass 8 
     #
     #   1 = whole note
     #   2 = half note
@@ -66,13 +66,13 @@ module Topaz
     # @param [Fixnum] interval
     # @return [Fixnum]
     def interval=(interval)
-      @per_tick = interval_to_ticks(interval)
+      @tick_threshold = interval_to_ticks(interval)
     end
     
     # Return the interval at which the tick event is fired
     # @return [Fixnum]
     def interval
-      ticks_to_interval(@per_tick)
+      ticks_to_interval(@tick_threshold)
     end
     
     private
@@ -80,7 +80,7 @@ module Topaz
     # Convert a note interval to number of ticks
     # @param [Fixnum] interval
     # @param [Fixnum]
-    def interval_to_tick(interval)
+    def interval_to_ticks(interval)
       per_qn = interval / 4
       24 / per_qn
     end
@@ -88,7 +88,7 @@ module Topaz
     # Convert a number of ticks to a note interval
     # @param [Fixnum] ticks
     # @param [Fixnum]
-    def tick_to_interval(ticks)
+    def ticks_to_interval(ticks)
       note_value = 24 / ticks
       4 * note_value
     end
@@ -112,12 +112,13 @@ module Topaz
     def handle_clock_message(message)
       thru
       log(message)
-      if tick?
-        tick
-        @counter = 0
-      else
-        @counter += 1
-      end
+      tick? ? tick : advance
+    end
+
+    # Advance the tick counter
+    # @return [Fixnum]
+    def advance
+      @tick_counter += 1
     end
 
     # Log the timestamp of a message for tempo calculation
@@ -132,19 +133,26 @@ module Topaz
     # (this results in MIDI output sending clock, thus thru)
     # @return [Boolean]
     def thru
-      !@event.nil? && !!@event.do_clock
+      if !@event.nil?
+        @event.do_clock
+        true
+      end
     end
 
     # Fire the tick event
     # @return [Boolean]
     def tick
-      !@event.nil? && !@pause && !!@event.do_tick
+      @tick_counter = 0
+      if !@event.nil? && !@pause
+        @event.do_tick
+        true
+      end
     end
 
     # Should the tick event be fired given the current state?
     # @return [Boolean]
     def tick?
-      @counter.eql?(@per_tick)
+      @tick_counter >= (@tick_threshold - 1)
     end
     
   end
