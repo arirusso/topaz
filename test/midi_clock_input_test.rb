b2f244d2141841fe77b054e0adca43d07a81be21
Ari Russo <ari.russo@gmail.com>
2014-09-24 18:26:43 -0700

    first pass at midi clock input test

---
+++ test/midi_clock_input_test.rb
@@ -0,0 +1,51 @@
+require "helper"
+
+class Topaz::MIDIClockInputTest < Test::Unit::TestCase
+
+  context "MIDIClockInput" do
+
+    setup do
+      @input = Object.new
+      @event = Object.new
+      @clock = Topaz::MIDIClockInput.new(@input, :event => @event)
+    end
+
+    context "#thru" do
+
+      should "fire clock event" do
+        @event.expects(:do_clock).once
+        assert @clock.send(:thru)
+      end
+
+    end
+
+    context "#tick" do
+
+      should "fire tick event" do
+        @event.expects(:do_tick).once
+        assert @clock.send(:tick)
+      end
+
+    end
+
+    context "#tick?" do
+
+      should "tick when counter has reached tick threshold" do
+        assert_equal 4, @clock.interval
+        23.times do 
+          refute @clock.send(:tick?)
+          @clock.send(:advance)
+        end
+        assert @clock.send(:tick?)
+        @clock.send(:advance)
+      end
+
+    end
+
+  end
+
+end
+
+
+
+
