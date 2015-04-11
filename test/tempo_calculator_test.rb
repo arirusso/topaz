a10b952c4e062c594061f7955d937865d8ea36c6
Ari Russo <ari.russo@gmail.com>
2015-04-11 13:08:39 -0400

    fix flaky test

--- test/tempo_calculator_test.rb
+++ test/tempo_calculator_test.rb
@@ -25,10 +25,10 @@ class Topaz::TempoCalculatorTest < Minitest::Test
       end
 
       should "express tempo" do
-        5.times { |i| @calc.timestamps << Time.now.to_f; sleep(1.0 / 24.0) }
+        5.times { |i| @calc.timestamps << i * (1.0 / 24.0) }
         result = @calc.calculate
         refute_nil result
-        assert (58..62).include?(result)
+        assert_equal 60, result
       end
 
     end
